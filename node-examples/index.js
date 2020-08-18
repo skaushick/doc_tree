7c4ded415dd0d12467d999de3aab283552d6961f
skaushick <sauravkaushick@gmail.com>
2020-08-18 15:57:20 +0530

    rectengle

---
+++ node-examples/index.js
@@ -0,0 +1,14 @@
+var rect = {
+        perimeter: (x,y) => (2*(x+y)),
+        area : (x,y) => (x*y)
+};
+
+function solverRect(l,b){
+    console.log("Rectengle solve");
+
+    if(l<=0){
+        console.log("negative scenario");
+    }
+}
+
+solverRect(4,5);
